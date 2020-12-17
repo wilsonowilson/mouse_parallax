5d3fb8b3911d5c8272185006610cc7a7c07c60a3
wilsonowilson <wilsonwilsonme@gmail.com>
2020-12-17 15:59:25 +0100

    Added meta

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,47 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:mouse_parallax/mouse_parallax.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MyApp());
 }
 
+/// Example app.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ParallaxStack(
-        resetOnExit: true,
-        layers: [
-          ParallaxLayer(
-            yRotation: -0.1,
-            xOffset: 50,
-            enable3D: true,
-            center: true,
-            child: Container(
-              width: 250,
-              height: 250,
-              color: Colors.black,
-            ),
-          ),
-          ParallaxLayer(
-            yRotation: -0.4,
-            enable3D: true,
-            center: true,
-            xOffset: 150,
-            yOffset: 30,
-            offset: const Offset(40, 40),
-            child: Container(
-              width: 200,
-              height: 200,
-              decoration: const BoxDecoration(
-                color: Colors.deepOrange,
+    return MaterialApp(
+      home: Scaffold(
+        body: ParallaxStack(
+          resetOnExit: true,
+          layers: [
+            ParallaxLayer(
+              yRotation: -0.1,
+              xOffset: 50,
+              enable3D: true,
+              center: true,
+              child: Container(
+                width: 250,
+                height: 250,
+                color: Colors.black,
               ),
             ),
-          ),
-        ],
-        referencePosition: 0.9999,
-        useLocalPosition: true,
-        touchBased: true,
-        detectPlatform: false,
+            ParallaxLayer(
+              yRotation: -0.4,
+              enable3D: true,
+              center: true,
+              offset: const Offset(40, 40),
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                  color: Colors.deepOrange,
+                ),
+              ),
+            ),
+          ],
+          referencePosition: 0.9999,
+          useLocalPosition: true,
+          touchBased: true,
+          detectPlatform: false,
+        ),
       ),
     );
   }
